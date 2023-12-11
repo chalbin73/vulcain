c5db91cb1dd5cf7d83e4048772b60388c98aa9e2
Albin Chaboissier <chalbin73@gmail.com>
2023-12-11 19:42:50 +0100

    Adding sanitize address in compilation

--- playground/premake5.lua
+++ playground/premake5.lua
@@ -9,6 +9,7 @@ toolset "clang"
 targetdir "bin"
 includedirs { "third_party/mathc", "third_party/fast_obj", "third_party/stb", "../third_party/vma/include", "../src" }
 libdirs { "third_party/stb", "../third_party/vma/lib", "../lib"}
-links { "m", "vulcain", "img", "glfw", "vulkan", "vma", "dl", "pthread", "stdc++" }
+buildoptions { "-fsanitize=address" }
+links { "m", "vulcain", "img", "glfw", "vulkan", "vma", "dl", "pthread", "stdc++" , "asan" }
 
 files { "src/**.c", "third_party/fast_obj/**.c", "third_party/mathc/**.c"}
--- premake5.lua
+++ premake5.lua
@@ -7,10 +7,10 @@ language "C"
 kind "StaticLib"
 toolset "clang"
 targetdir "lib"
-buildoptions { "-Wall", "-Werror", "-g" }
+buildoptions { "-Wall", "-Werror", "-g", "-fsanitize=address" }
 includedirs { "third_party/vma/include" }
 libdirs { "third_party/vma/lib" }
-links { "m", "glfw", "vulkan", "dl", "pthread", "X11", "Xxf86vm", "Xrandr", "Xi"}
+links { "m", "glfw", "vulkan", "dl", "pthread", "X11", "Xxf86vm", "Xrandr", "Xi", "asan"}
 
 files { "src/**.c" }
 
